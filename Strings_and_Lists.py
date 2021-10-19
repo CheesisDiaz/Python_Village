75d80608c2a49022aefaaecd6f89cf58bb6f78d1
CheesisDiaz <89626045+CheesisDiaz@users.noreply.github.com>
2021-10-18 18:32:24 -0700

    Add files via upload
    Problem
    Given: A string s of length at most 200 letters and four integers a, b, c and d.
    
    Return: The slice of this string from indices a through b and c through d (with space in between), inclusively. In other words, we should include elements s[b] and s[d] in our slice.

---
+++ Strings_and_Lists.py
@@ -0,0 +1,9 @@
+string = "HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain."
+a = 22
+b = 27
+c = 97
+d = 102
+
+New_string = string[a: b+1] + " " + string[c: d+1]
+
+print(New_string)
